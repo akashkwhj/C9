15113d484709757dbf54bb0c3168f6b591c8b7c5
akashkwhj <99476787+akashkwhj@users.noreply.github.com>
2022-02-15 03:00:48 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,20,20);
+
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown(RIGHT_ARROW)){
+    box.x=box.x+5;
+  }
+  if(keyDown(UP_ARROW)){
+     box.y=box.y-5
+  }
+  drawSprites();
+  
+}
+
+
+
+
